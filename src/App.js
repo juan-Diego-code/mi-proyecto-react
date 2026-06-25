5c7eacef9e5fcc265c8ac11e4db80730dfcbe062
Juan Diego <juandcortesa11@gmail.com>
2026-06-25 15:29:22 -0500

    Primera version

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Este es mi primer proyecto en React, estoy aprendiendo a usarlo y me parece muy interesante.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/juan-Diego-code"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mi GitHub
         </a>
       </header>
     </div>
